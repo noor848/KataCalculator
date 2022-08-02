100946fb25f9dfb44b74cfde9f5f4dffab19fc63
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-02 23:41:19 +0300

    Revert "TAX section Done"
    This reverts commit f9a7e6704cd35bb56a3888b206ed11e419865d2d.

---
+++ Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Calculator
+{
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public string UPC { get; set; }
+        public double ProductPrice { get; set; }
+        public double PriceAfterTax { get; set; }
+        public double PriceAfterDiscount { get; set; }
+        public  string currencyOfProductSymbol { get; set; }
+        public  double PriceAfterConfigureDiscount { get; set; }
+        public double UPCdiscount { get; set; } = 7;
+
+        public Product(string name, string upc, double price, string currency)
+        {
+            Name = name;
+            UPC = upc;
+            ProductPrice = price;
+            PriceAfterTax = Tax.TaxCalculation(price)+price;
+            currencyOfProductSymbol = currency;
+        }
+        public double UPCDiscount(double price)
+        {
+            if (Stock.SpecialUPC.Contains(UPC))
+            {
+                return (UPCdiscount / 100) * price;
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            return 0;
+        }
+    }
+}
---
+++ Stock.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Calculator
+{
+    public class Stock
+    {
+        public  static List<Product> Items;
+        public static List<string> SpecialUPC;
+        public Stock()
+        {
+            Items = new List<Product>();
+            SpecialUPC = new List<string>(){"1111","2222","3333"};
+        }
+        public  void EnterProductDetails(int itemNumber)
+        {
+            int i = 0;
+            while (i < itemNumber)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine($"\nProduct Num: #{i + 1}\n");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Enter Name of Product ");
+                string? NameOfProduct = Console.ReadLine();
+
+                Console.WriteLine("Enter 4 character of UPC of Product ");
+
+                string ? UPC = Console.ReadLine();
+                if(UPC!.Length<4 || UPC!.Length > 4 || UPC.Length == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please check the Length of UPC\n TRY AGAIN!");
+                    continue;
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("Enter Price of Product ");
+                double PriceOfProduct = Convert.ToDouble(Console.ReadLine());
+                if (PriceOfProduct == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please check the Price of Product!");
+                    continue;
+                }
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Enter Currency of Product (US,Jordan,Shekel)");
+                string? currencyOfProduct = Console.ReadLine();
+                try
+                {
+                    currencyOfProduct = Currency.CurrencyType(currencyOfProduct!);
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Enter one of these types(US,Jordan,Shekel)\n TRY AGAIN !");
+                    continue;
+                }
+                Items.Add(new Product(NameOfProduct!, UPC, PriceOfProduct, currencyOfProduct));
+                i++;
+            }
+
+
+
+        }
+   
+    }
+}
