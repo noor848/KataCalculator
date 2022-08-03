970baa6d5db332585da7f64779a01137016f8a3c
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-04 00:24:47 +0300

    CAP section done

---
+++ Cap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Calculator
+{
+    public class Cap
+    {
+        public  static void AddingCapDiscount(string upc,double CapAmount)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Product product = Product.GetProductWithSpecifiedUPC(upc);
+            product.TotalDiscount = product.ProductPrice * CapAmount;
+            Report.ReportTotalExpenses(product);
+        }
+    }
+}
