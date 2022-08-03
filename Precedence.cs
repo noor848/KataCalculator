6e8cef89a84a90f29b913b483778960026d305d7
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-03 20:45:34 +0300

    PRECEDENCE work edited

---
+++ Precedence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Calculator
+{
+    public class Precedence
+    {
+        public static void preceding(string upc)
+        {
+            Product product = Stock.Items.First();
+            product.RemainingPrice = product.ProductPrice - product.UPCDiscount(product.ProductPrice);
+            product.TaxAmount = ((product.RemainingPrice * Tax.TaxRate)/100);
+            double FinalProduct = product.RemainingPrice + product.TaxAmount - product.UniversalDiscountAmount(product.RemainingPrice);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(Report.TwoDecimalPlaces(FinalProduct));
+            Console.WriteLine($"Program Reports Total Discount Amount For " +
+                $"UPC = {upc} Is { Report.TwoDecimalPlaces(product.UniversalDiscountAmount(product.RemainingPrice)+ product.UPCDiscount(product.ProductPrice))}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
