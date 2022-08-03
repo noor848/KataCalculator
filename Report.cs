f963372a9db24e6b18657711b3ce3247365b5772
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-03 20:44:39 +0300

    REPORT section edited

--- Report.cs
+++ Report.cs
@@ -10,15 +10,15 @@ namespace Kata_Calculator
     {
         public static void ReportBeforeDiscount()
         {
-            string PriceSymbole = "";
-            Console.ForegroundColor = ConsoleColor.Green;
-
+            Console.ForegroundColor = ConsoleColor.Yellow;
             foreach (var item in Stock.Items)
             {
-                Console.WriteLine($"\n Product Name is {item.Name} and UPC {item.UPC} price reported as before tax {item.ProductPrice}{item.currencyOfProductSymbol} and {item.PriceAfterTax.ToString("0.00")}{item.currencyOfProductSymbol} after tax {Tax.TaxRate}%");
+                Console.WriteLine($"\n Product Name is {item.Name} and UPC {item.UPC} " +
+                    $"price reported as before tax {item.ProductPrice}{item.currencyOfProductSymbol}" +
+                    $" and {TwoDecimalPlaces(item.PriceAfterTax)}{item.currencyOfProductSymbol}" +
+                    $" after tax {Tax.TaxRate}%\n");
             }
             Console.ForegroundColor = ConsoleColor.White;
-
         }
         public static void ReportAfterDiscount()
         {
@@ -27,12 +27,62 @@ namespace Kata_Calculator
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine($"\n Product Name is {product.Name} and UPC {product.UPC}" +
                     $" price reported as before tax {product.ProductPrice}{product.currencyOfProductSymbol} " +
-                    $"and {product.PriceAfterTax.ToString("0.00")}{product.currencyOfProductSymbol} after tax {Tax.TaxRate}%" +
-                    $"and discount {UniversalDiscountnumber}% and Product Price After discount is {product.PriceAfterDiscount.ToString("0.00") ?? "no discount"}{product.currencyOfProductSymbol}");
+                    $"and {TwoDecimalPlaces(product.PriceAfterTax)}{product.currencyOfProductSymbol} after tax {Tax.TaxRate}%" +
+                    $"and discount {product.UniversalDiscountnumber}% and Product Price After discount is {TwoDecimalPlaces(product.PriceAfterDiscount)}{product.currencyOfProductSymbol}\n");
                 Console.ForegroundColor = ConsoleColor.White;
             }
+        }
+        public static void ReportNoDiscount()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            foreach (var product in Stock.Items)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"\n Product Name is {product.Name} and UPC {product.UPC}" +
+                    $" price reported as before tax {product.ProductPrice}{product.currencyOfProductSymbol} " +
+                    $"and {product.PriceAfterTax.ToString("0.00")}{product.currencyOfProductSymbol} after tax {Tax.TaxRate}%" +
+                    $"and discount {product.UniversalDiscountnumber}% and No discount ! ");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.ForegroundColor = ConsoleColor.White;
 
         }
+        public static void ReportPrintSelective(Product product)
+        {
+            product.PriceAfterConfigureDiscount = product.ProductPrice - product.UniversalDiscountAmount(product.ProductPrice) -
+                  product.UPCDiscount(product.ProductPrice) + Tax.TaxCalculation(product.ProductPrice);
 
+            product.TotalDiscount = product.UniversalDiscountAmount(product.ProductPrice) + product.UPCDiscount(product.ProductPrice);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            if (product.UPCDiscount(product.ProductPrice) == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            Console.WriteLine($"\n Product Name is {product.Name} and UPC {product.UPC}" +
+                $" price reported as before tax {product.ProductPrice}{product.currencyOfProductSymbol} " +
+                $"and {TwoDecimalPlaces(product.PriceAfterTax)}{product.currencyOfProductSymbol}\n after tax {Tax.TaxRate}%" +
+                $"and discount {product.UniversalDiscountnumber}%" +
+                $" and Product Price After discount is {product.PriceAfterDiscount.ToString("0.00")}{product.currencyOfProductSymbol}" +
+                $"and specific discount {product.UPCDiscount(product.ProductPrice).ToString("0.00")}% and Product Price After discount {product.PriceAfterConfigureDiscount.ToString("0.00")}{product.currencyOfProductSymbol}\n" +
+                $" Program reports total discount amount {TwoDecimalPlaces(product.TotalDiscount)}\n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static string TwoDecimalPlaces(double text)
+        {
+            return text.ToString("0.00");
+        }
+        public static void ReportTotalExpenses(Product product)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Product Cost = {TwoDecimalPlaces(product.ProductPrice)}");
+            Console.WriteLine($"Tax = {TwoDecimalPlaces(product.TaxAmount)}");
+            Console.WriteLine($"Discounts = {TwoDecimalPlaces(product.TotalDiscount)}");
+            Console.WriteLine($"Packaging = {TwoDecimalPlaces(product.PackagingCost)}");
+            Console.WriteLine($"Transport = {TwoDecimalPlaces(product.TransportCost)}");
+            product.TotalExpenses = product.ProductPrice + product.TaxAmount - product.TotalDiscount + product.PackagingCost + product.TransportCost;
+            Console.WriteLine($"Total Expenses = {TwoDecimalPlaces(product.TotalExpenses)}");
+        }
     }
 }
+
