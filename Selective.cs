37bf06d9dc1f028b4aeff82969f541a5872845f2
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-03 20:46:44 +0300

    SELECTIVE section fixed

--- Selective.cs
+++ Selective.cs
@@ -8,31 +8,14 @@ namespace Kata_Calculator
 {
     public class Selective
     {
-        public static void Selective(string upc)
+        public static void selective(string upc)
         {
-            List<Product> SpecificProduct = Stock.Items.Where(s => s.UPC.Equals(upc)).ToList();
-            Product product = SpecificProduct.First();
-
+            Product product = Stock.Items.First();
             if (product.UPCDiscount(product.ProductPrice) == 0)
             {
-                Console.WriteLine(" Not Special UPC ");
+                Console.Write("\n Not Special UPC\n");
             }
-
-            product.PriceAfterConfigureDiscount = product.ProductPrice - Discount.UniversalDiscountAmount(product.ProductPrice) -
-                    product.UPCDiscount(product.ProductPrice) + Tax.TaxCalculation(product.ProductPrice);
-            double TotalDiscount = Discount.UniversalDiscountAmount(product.ProductPrice) + product.UPCDiscount(product.ProductPrice);
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-
-            Console.WriteLine($"\n Product Name is {product.Name} and UPC {product.UPC}" +
-                $" price reported as before tax {product.ProductPrice}{product.currencyOfProductSymbol} " +
-                $"and {product.PriceAfterTax.ToString("0.00")}{product.currencyOfProductSymbol}\n after tax {Tax.TaxRate}%" +
-                $"and discount {Discount.UniversalDiscountnumber}%" +
-                $" and Product Price After discount is {product.PriceAfterDiscount.ToString("0.00")}{product.currencyOfProductSymbol}" +
-                $"and specific discount {product.UPCDiscount(product.ProductPrice).ToString("0.00")}% and Product Price After discount {product.PriceAfterConfigureDiscount.ToString("0.00")}{product.currencyOfProductSymbol}\n" +
-                $" Program reports total discount amount {TotalDiscount.ToString("0.00")}");
-
-            Console.ForegroundColor = ConsoleColor.White;
+            Report.ReportPrintSelective(product);
         }
     }
 }
