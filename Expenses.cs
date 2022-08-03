e3082f06d94f9f594a55dd7cd2f750f77f137e3f
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-03 20:44:59 +0300

    EXPENSES work done

---
+++ Expenses.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Calculator
+{
+    public  class Expenses
+    {
+        public static void TotalExpenses(string upc)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Product product = Stock.Items.First();
+
+            Console.WriteLine("Do you Want To Transport?");
+            if (Console.ReadLine().ToLower().Equals("yes"))
+            {
+                double PercentPackagingCost = .01;
+                product.PackagingCost = PercentPackagingCost * product.ProductPrice;
+                product.TransportCost = 2.2;
+            }
+            product.TaxAmount = Tax.TaxCalculation(product.ProductPrice);
+            Report.ReportTotalExpenses(product);
+        }
+
+    }
+}
